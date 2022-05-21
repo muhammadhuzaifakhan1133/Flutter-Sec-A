b89b1a5895e3c2b6b28f991b5bee0a60644a1574
Muhammad Huzaifa Khan <huzaifaacademy11@gmail.com>
2022-05-21 13:06:20 +0500

    Clear Render problem

--- Flutter/Projects/scrolllayout/lib/screens/home.dart
+++ Flutter/Projects/scrolllayout/lib/screens/home.dart
@@ -8,69 +8,122 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(actions: [Icon(Icons.logout)], title: Text("Scrolling in both Directions"),),
+      appBar: AppBar(
+        actions: [Icon(Icons.logout)],
+        title: Text("Scrolling in both Directions"),
+      ),
       body: SingleChildScrollView(
         child: Column(
           children: [
             Container(
-              width: double.infinity, 
+                width: double.infinity,
+                height: 200,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 15, left: 15),
+                        width: 200,
+                        color: Colors.blue,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 15, left: 15),
+                        width: 200,
+                        color: Colors.orange,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 15, left: 15),
+                        width: 200,
+                        color: Colors.red,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 15, left: 15, right: 15),
+                        width: 200,
+                        color: Colors.green,
+                      ),
+                    ],
+                  ),
+                )),
+            Container(
+              width: double.infinity,
               height: 200,
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 15),
+              width: double.infinity,
+              height: 200,
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 15),
+                    width: 157,
+                    color: Colors.blue[300],
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 15, right: 15),
+                    width: 157,
+                    color: Colors.brown,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 15),
+              width: double.infinity,
+              height: 200,
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 15),
+                    width: 157,
+                    color: Colors.deepOrange,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 15, right: 15),
+                    width: 157,
+                    color: Colors.deepPurple,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.only(top: 15),
+                width: double.infinity,
+                height: 200,
                 child: Row(
                   children: [
-                    Container(margin: EdgeInsets.only(top:15, left:15), width: 200, color: Colors.blue,),
-                    Container(margin: EdgeInsets.only(top:15, left:15), width: 200, color: Colors.orange,),
-                    Container(margin: EdgeInsets.only(top:15, left:15), width: 200, color: Colors.red,),
-                    Container(margin: EdgeInsets.only(top:15, left:15, right:15), width: 200, color: Colors.green,),                  ],
-                ),
-              )
-            ),
-            Container(width: double.infinity, height: 200,),
-            Container(
-              margin: EdgeInsets.only(top: 15),
-              width: double.infinity, 
-              height: 200,
-              child: Row(
-                children: [
-                  Container(margin: EdgeInsets.only(left: 15), width: 169.5, color: Colors.blue[300],),
-                  Container(margin: EdgeInsets.only(left: 15, right: 15), width: 169.5, color: Colors.brown,),
-                ],
-              ),
-              ),
-            Container(
-              margin: EdgeInsets.only(top: 15),
-              width: double.infinity, 
-              height: 200,
-              child: Row(
-                children: [
-                  Container(margin: EdgeInsets.only(left: 15), width: 169.5, color: Colors.deepOrange,),
-                  Container(margin: EdgeInsets.only(left: 15, right: 15), width: 169.5, color: Colors.deepPurple,), 
-                ],
-              ),
-              ),
-            Container(
-              margin: EdgeInsets.only(top: 15),
-              width: double.infinity, 
-              height: 200,
-              child: Row(
-                children: [
-                  Container(margin: EdgeInsets.only(left: 15), width: 169.5, color: Colors.green,),
-                  Container(margin: EdgeInsets.only(left: 15, right: 15), width: 169.5, color: Colors.blueGrey,)
-                ],
-              )
-              ),
+                    Container(
+                      margin: EdgeInsets.only(left: 15),
+                      width: 157,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 15, right: 15),
+                      width: 157,
+                      color: Colors.blueGrey,
+                    )
+                  ],
+                )),
             Container(
               margin: EdgeInsets.only(top: 15, bottom: 15),
-              width: double.infinity, 
+              width: double.infinity,
               height: 200,
               child: Row(
                 children: [
-                  Container(margin: EdgeInsets.only(left: 15), width: 169.5, color: Colors.brown,),
-                  Container(margin: EdgeInsets.only(left: 15, right: 15), width: 169.5, color: Colors.deepPurple,),
+                  Container(
+                    margin: EdgeInsets.only(left: 15),
+                    width: 157,
+                    color: Colors.brown,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 15, right: 15),
+                    width: 157,
+                    color: Colors.deepPurple,
+                  ),
                 ],
               ),
-              ),
+            ),
           ],
         ),
       ),
